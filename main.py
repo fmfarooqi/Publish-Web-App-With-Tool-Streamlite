377afafa54abe8d2f6ebdb9f087989ebcd3b7de4
fmfarooqi <155878156+fmfarooqi@users.noreply.github.com>
2024-02-15 19:52:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Every Thing With python')
+st.write('''This is our first web app.
+Learn it and enjoy it!
+''')
